28d42dcbefbdf7272d7d51d648a14412fbb1a379
MnhT <minh.huynh@edu.tbz.ch>
2023-11-17 09:57:10 +0100

    Test

---
+++ D/BankSimulation.java
@@ -0,0 +1,27 @@
+package D;
+
+import D.Konto;
+
+// Testklasse
+public class BankSimulation {
+    public static void Main(String[] args) {
+        // Drei verschiedene Konten erstellen
+        Konto konto1 = new Konto(1, 1000);
+        Konto konto2 = new Konto(2, 1500);
+        Konto konto3 = new Konto(3, 2000);
+
+        // Einzahlung auf Konto 1
+        konto1.einzahlen(500);
+
+        // Überweisung von Konto 2 auf Konto 3
+        konto2.ueberweisen(konto3, 200);
+
+        // Abhebung von Konto 3
+        konto3.abheben(100);
+
+        // Ausgabe der Kontostände
+        System.out.println("Konto " + konto1.getKontoNr() + ": Saldo = " + konto1.getSaldo());
+        System.out.println("Konto " + konto2.getKontoNr() + ": Saldo = " + konto2.getSaldo());
+        System.out.println("Konto " + konto3.getKontoNr() + ": Saldo = " + konto3.getSaldo());
+    }
+}
---
+++ D/Konto.java
@@ -0,0 +1,51 @@
+package D;
+
+public class Konto {
+    private int kontoNr;
+    private double saldo;
+
+    // Konstruktor mit Startguthaben von 100
+    public Konto(int kontoNr, int Saldo) {
+        this.kontoNr = kontoNr;
+        this.saldo = Saldo;
+    }
+
+    // Getter für KontoNr
+    public int getKontoNr() {
+        return kontoNr;
+    }
+
+    // Getter für Saldo
+    public double getSaldo() {
+        return saldo;
+    }
+
+    // Einzahlen-Methode
+    public void einzahlen(double betrag) {
+        saldo += betrag;
+        System.out.println("Einzahlung von " + betrag + " auf Konto " + kontoNr + ". Neuer Saldo: " + saldo);
+    }
+
+    // Abheben-Methode
+    public void abheben(double betrag) {
+        if (saldo >= betrag) {
+            saldo -= betrag;
+            System.out.println("Abhebung von " + betrag + " von Konto " + kontoNr + ". Neuer Saldo: " + saldo);
+        } else {
+            System.out.println("Nicht genug Guthaben auf Konto " + kontoNr);
+        }
+    }
+
+    // Überweisen-Methode
+    public void ueberweisen(Konto nachKonto, double betrag) {
+        if (saldo >= betrag) {
+            abheben(betrag);
+            nachKonto.einzahlen(betrag);
+            System.out.println("Überweisung von Konto " + kontoNr + " nach Konto " + nachKonto.getKontoNr() + " erfolgreich.");
+        } else {
+            System.out.println("Nicht genug Guthaben auf Konto " + kontoNr + " für die Überweisung.");
+        }
+    }
+}
+
+
